b4aaa439ffc021e0a5aa9ab6fc1f65a65ecd1836
PierreFrs <pierrefraisse90@gmail.com>
2023-02-08 14:39:44 +0100

    quotes

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+// let apiQuotes = [];
 
-    // Get Quotes From API
-    async function getQuotes() {
-        ...
-        const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-        try {
-        ...
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from apiQuote array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes From API
+// async function getQuotes() {
+//     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     } catch (error) {
+//         //Catch Error Here
+//     }
+// }
+
+// On Load
+// getQuotes();
+newQuote();
